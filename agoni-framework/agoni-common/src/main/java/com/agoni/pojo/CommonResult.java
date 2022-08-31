de315e061f72f895b8c3dc3bd9b584b384267dc4
AgoniMou <2627045617@qq.com>
2022-08-31 17:50:30 +0800

    fix 更改框架架构

---
+++ agoni-framework/agoni-common/src/main/java/com/agoni/pojo/CommonResult.java
@@ -0,0 +1,51 @@
+package com.agoni.pojo;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import org.springframework.util.Assert;
+
+import java.io.Serializable;
+
+/**
+ * 通用响应
+ *
+ * @param <T>
+ * @author AgoniMou
+ */
+@Data
+public class CommonResult<T> implements Serializable {
+
+
+    /**
+     * 响应时间戳
+     */
+    @ApiModelProperty("响应时间戳")
+    private Long timestamp;
+
+
+    /**
+     * 响应码
+     */
+    @ApiModelProperty("响应码")
+    private Integer code;
+
+    /**
+     * 响应数据
+     */
+    @ApiModelProperty("响应数据")
+    private T data;
+
+    /**
+     * 错误提示
+     */
+    @ApiModelProperty("错误提示，用户可阅读")
+    private String msg;
+
+    public static <T> CommonResult<T> error(Integer code, String message) {
+//        Assert.isTrue(!GlobalErrorCodeConstants.SUCCESS.getCode().equals(code), "code 必须是错误的！");
+        CommonResult<T> result = new CommonResult<>();
+        result.code = code;
+        result.msg = message;
+        return result;
+    }
+}
