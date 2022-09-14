e042eb6f98581a289834a78abddb18b11343a609
AgoniMou <2627045617@qq.com>
2022-09-14 19:50:12 +0800

    add 新增druid底部广告过滤

--- agoni-framework/agoni-spring-boot-starter-mybatis/src/main/java/com/agoni/mybatis/config/MybatisAutoConfiguration.java
+++ agoni-framework/agoni-spring-boot-starter-mybatis/src/main/java/com/agoni/mybatis/config/MybatisAutoConfiguration.java
@@ -1,7 +1,11 @@
 package com.agoni.mybatis.config;
 
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.apache.ibatis.annotations.Mapper;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -13,5 +17,22 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class MybatisAutoConfiguration {
 
+    /**
+     * 分页拦截器
+     * @return 分页拦截器
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor()); // 分页插件
+        return mybatisPlusInterceptor;
+    }
 
+    /**
+     * 自动填充配置
+     */
+    @Bean
+    public MetaObjectHandler defaultMetaObjectHandler() {
+        return new MybatisPlusMetaObjectHandler(); // 自动填充参数类
+    }
 }
--- agoni-framework/agoni-spring-boot-starter-mybatis/src/main/java/com/agoni/mybatis/core/entity/BaseEntity.java
+++ agoni-framework/agoni-spring-boot-starter-mybatis/src/main/java/com/agoni/mybatis/core/entity/BaseEntity.java
@@ -2,15 +2,16 @@ package com.agoni.mybatis.core.entity;
 
 import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableLogic;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.apache.ibatis.type.JdbcType;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.io.Serializable;
-import java.time.LocalDateTime;
 import java.util.Date;
 
 /**
@@ -20,16 +21,14 @@ import java.util.Date;
  * @since 2022-09-14
  */
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
-public class BaseEntity implements Serializable {
+public abstract class BaseEntity implements Serializable {
 
     private static final long serialVersionUID = 1L;
     /**
      * 创建者
      */
-    @ApiModelProperty(value = "创建者",hidden = true)
-    @TableField(fill = FieldFill.INSERT)
+    @ApiModelProperty(value = "创建者", hidden = true)
+    @TableField(fill = FieldFill.INSERT, jdbcType = JdbcType.VARCHAR)
     private String createBy;
 
     /**
@@ -45,7 +44,7 @@ public class BaseEntity implements Serializable {
      * 更新者
      */
     @ApiModelProperty("更新者")
-    @TableField(fill = FieldFill.INSERT_UPDATE)
+    @TableField(fill = FieldFill.INSERT_UPDATE, jdbcType = JdbcType.VARCHAR)
     private String updateBy;
 
     /**
@@ -56,4 +55,11 @@ public class BaseEntity implements Serializable {
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     @TableField(fill = FieldFill.INSERT_UPDATE)
     private Date updateTime;
+
+
+    /**
+     * 是否删除
+     */
+    @TableLogic
+    private Boolean deleted;
 }
