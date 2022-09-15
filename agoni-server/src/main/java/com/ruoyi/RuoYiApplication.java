ed775fe76f4b26963b0056b5830bdf379ac81b64
AgoniMou <2627045617@qq.com>
2022-09-15 16:19:54 +0800

    :art: 修改agoni-admin改名为agoni-server,为后续开发做准备

--- agoni-server/src/main/java/com/ruoyi/RuoYiApplication.java
+++ agoni-server/src/main/java/com/ruoyi/RuoYiApplication.java
@@ -1,11 +1,14 @@
 package com.ruoyi;
 
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * 启动程序
  *
- * @author ruoyi
+ * @author AgoniMou
  */
+@SpringBootApplication
 public class RuoYiApplication {
     public static void main(String[] args) {
 
